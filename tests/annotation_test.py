49ded0ade8844f5c6b423e9cb14c7dd13be78d8f
Joshua Scheck <49527681+jschec@users.noreply.github.com>
2021-09-22 13:18:05 -0700

    Created additional testing fixtures.

--- tests/annotation_test.py
+++ tests/annotation_test.py
@@ -6,24 +6,41 @@ import pytest
 def moclo_assemblies():
     return [
         ("seq1", "part", []),
-        ("seq2", "cassette", [])
+        ("seq2", "part", []),
+        ("seq3", "cassette", []),
+        ("seq4", "cassette", [])
     ]
 
+@pytest.fixture(scope='class')
+def invalid_moclo_assemblies():
+    return [
+        ("seq1", "", []),
+        ("seq2", "multicassette", [])
+    ]
 
-def test_annotate_moclo(moclo_assembly: Tuple[str, str, List[str]]):
+def test_annotate_moclo(moclo_assemblies: List[Tuple[str, str, List[str]]]):
     """[summary]
     """
-    dna_sequence = moclo_assembly[0]
-    annotation_type = moclo_assembly[1]
-    expected_result = moclo_assembly[2]
 
-    actual_result = annotate_moclo(dna_sequence, annotation_type)
+    for moclo_assembly in moclo_assemblies:
 
-    assert len(actual_result) == len(expected_result)
-    assert annotate_moclo(dna_sequence, annotation_type) == expected_result
+        dna_sequence = moclo_assembly[0]
+        annotation_type = moclo_assembly[1]
+        expected_result = moclo_assembly[2]
 
-def test_annotate_moclo_exception():
+        actual_result = annotate_moclo(dna_sequence, annotation_type)
+
+        assert len(actual_result) == len(expected_result)
+        assert annotate_moclo(dna_sequence, annotation_type) == expected_result
+
+def test_annotate_moclo_exception(invalid_moclo_assemblies: List[Tuple[str, str, List[str]]]):
     """[summary]
     """
-    with pytest.raises(Exception):
-        annotate_moclo('', 'multicassette')
+    for moclo_assembly in invalid_moclo_assemblies:
+
+        dna_sequence = moclo_assembly[0]
+        annotation_type = moclo_assembly[1]
+        expected_result = moclo_assembly[2]
+
+        with pytest.raises(Exception):
+            actual_result = annotate_moclo(dna_sequence, annotation_type)
